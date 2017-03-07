658854b8d971788e28c4a6afb61a65f140883ed1
Ben Armston <ben.armston@alces-software.com>
2017-03-07 20:33:57 +0000

    Add some tests

--- src/Blurb.test.js
+++ src/Blurb.test.js
@@ -7,9 +7,9 @@
  *===========================================================================*/
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Blurb from './Blurb';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Blurb />, div);
 });
--- src/ClusterCard.test.js
+++ src/ClusterCard.test.js
@@ -7,9 +7,15 @@
  *===========================================================================*/
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import ClusterCard from './ClusterCard';
 
 it('renders without crashing', () => {
+  const clusterSpec = {
+    title: 'Some title',
+    subtitle: 'Some title',
+    body: 'Some content',
+    logoUrl: 'http://example.com/logo.png',
+  };
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<ClusterCard clusterSpec={clusterSpec}  />, div);
 });
