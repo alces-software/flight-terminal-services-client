e81aef8d83fab8d6b4fc72bcdc8c8980dcde60fb
Ben Armston <ben.armston@alces-software.com>
2017-03-31 16:13:12 +0100

    Update status of tokens when they get used

--- server/app/models/token.rb
+++ server/app/models/token.rb
@@ -11,6 +11,26 @@ class Token
 
   attr_accessor :token_string
 
+  class << self
+    def create_new_token(token_string)
+      client.put_item({
+        item: {
+          "Token" => token_string, 
+          "Status" => "AVAILABLE", 
+        }, 
+        table_name: "FlightLaunchTokens", 
+        condition_expression: "attribute_not_exists(#token)",
+        expression_attribute_names: {
+          "#token": "Token",
+        },
+      })
+    end
+
+    def client
+      Aws::DynamoDB::Client.new(region: 'eu-west-1')
+    end
+  end
+
   def not_found?
     status == 'NOT_FOUND'
   end
@@ -27,6 +47,23 @@ class Token
     end
   end
 
+  def mark_as(s, used_by)
+    attrs = {
+      "Token" => token_string,
+      "Status" => s.to_s.upcase,
+    }
+    if s == :used
+      attrs.merge!({
+        "UsedBy" => used_by,
+        "UsedAt" => DateTime.now.to_s
+      })
+    end
+    client.put_item({
+      item: attrs,
+      table_name: "FlightLaunchTokens", 
+    })
+  end
+
   private
 
   def token
@@ -37,6 +74,6 @@ class Token
   end
 
   def client
-    @client ||= Aws::DynamoDB::Client.new(region: 'eu-west-1')
+    @client ||= self.class.client
   end
 end
