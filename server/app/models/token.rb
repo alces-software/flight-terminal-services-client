63c6523ada71b96843947d45c68d88844ed86ce8
Ben Armston <ben.armston@alces-software.com>
2017-04-25 13:19:35 +0100

    Better tracking of token usage
    Change UsedAt -> UpdatedAt. Add UsedBy for IN_USE status.

--- server/app/models/token.rb
+++ server/app/models/token.rb
@@ -58,28 +58,22 @@ class Token
     params = {
       expression_attribute_names: {
         "#status": "Status",
+        "#updated_at": "UpdatedAt",
+        "#used_by": "UsedBy",
       },
       expression_attribute_values: {
         ":status": s.to_s.upcase,
+        ":updated_at": DateTime.now.to_s,
+        ":used_by": s == :available ? nil: used_by,
       },
       key: {
         "Token": token_string,
       },
       return_values: "ALL_NEW",
       table_name: "FlightLaunchTokens", 
-      update_expression: "SET #status = :status",
+      update_expression: "SET #status = :status, #updated_at = :updated_at, #used_by = :used_by",
     }
-    if s == :used
-      params[:expression_attribute_names].merge!(
-        "#used_by": "UsedBy",
-        "#used_at": "UsedAt",
-      )
-      params[:expression_attribute_values].merge!(
-        ":used_by": used_by,
-        ":used_at": DateTime.now.to_s,
-      )
-      params[:update_expression] = "SET #status = :status, #used_by = :used_by, #used_at = :used_at"
-    end
+    Alces.app.logger.info("Updating token #{token_string}: #{params[:expression_attribute_values]}")
     client.update_item(params)
   end
 
