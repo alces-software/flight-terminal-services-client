426ace827369b83fddf791f017c98bee2a529418
Ben Armston <ben.armston@alces-software.com>
2017-05-16 15:19:12 +0100

    Add seeds for tenants

---
+++ server/lib/alces/setup/seeder.rb
@@ -0,0 +1,39 @@
+#==============================================================================
+# Copyright (C) 2017 Stephen F. Norledge and Alces Flight Ltd.
+#
+# This file is part of Alces Launch.
+#
+# All rights reserved, see LICENSE.txt.
+#==============================================================================
+module Alces
+  module Setup
+    class Seeder
+      class << self
+        def seed
+          new.seed
+        end
+      end
+
+      def seed
+        seed_tenants
+      end
+
+      def seed_tenants
+        Alces.app.logger("Creating tenants")
+        Tenant.create!(
+          identifier: "bigvuni",
+          name: "University of Big V",
+          description: "The University of Big V",
+          logo_url: "https://s3.amazonaws.com/vlj/UNIVERSITY+OF+BIG+V+(tiny).png",
+          remaining_credits: 0
+        )
+        Tenant.create!(
+          identifier: "challenge",
+          name: "Alces Flight Launch Challenge",
+          description: "Tenant for the Alces Flight Launch challenge",
+          remaining_credits: 0
+        )
+      end
+    end
+  end
+end
