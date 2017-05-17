4cda862b91c97cba059ad876e92f5b6679086e1e
Ben Armston <ben.armston@alces-software.com>
2017-05-17 16:24:05 +0100

    Tenants have a specs url config

--- server/app/resources/api/v1/tenant_resource.rb
+++ server/app/resources/api/v1/tenant_resource.rb
@@ -8,6 +8,7 @@
 
 class Api::V1::TenantResource < Api::V1::ApplicationResource
   attribute :admin_email
+  attribute :cluster_specs_url_config
   attribute :description
   attribute :header
   attribute :home_page_url
@@ -17,4 +18,14 @@ class Api::V1::TenantResource < Api::V1::ApplicationResource
   attribute :nav_entry
 
   filter :identifier
+
+  def cluster_specs_url_config
+    {
+      default_file: @model.default_cluster_specs_file,
+      default_url: @model.cluster_specs_url,
+      prefix: @model.cluster_specs_url_prefix,
+    }
+    .deep_stringify_keys
+    .deep_transform_keys{|k| k.camelize(:lower)}
+  end
 end
