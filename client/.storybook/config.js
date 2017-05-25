c1a20bc5210a5025fae26374eb517f2c9b8d80e8
Ben Armston <ben.armston@alces-software.com>
2017-05-25 16:39:35 +0100

    Fix stories; move inside modules

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,12 +1,14 @@
 import { configure } from '@kadira/storybook';
 
 const req = require.context('../src/stories', true, /.js$/)
+const reqModules = require.context('../src/modules/', true, /.story.js$/)
 
 function loadStories() {
   if (req.keys().includes('./index.js')) {
     req('./index.js');
   }
-  req.keys().forEach((filename) => req(filename))
+  req.keys().forEach(req);
+  reqModules.keys().forEach(reqModules)
 }
 
 configure(loadStories, module);
