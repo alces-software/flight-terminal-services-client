2cc3916c905d6d8731234e61da6b61787f488696
Ben Armston <ben.armston@alces-software.com>
2017-07-19 16:01:47 +0100

    Prevent loading multiple copies of react
    `flight-reactware` includes React as a dev dependency, but not as a
    dependency.  `flight-launch` depends on `flight-reactware`.
    
    Under normal `yarn add flight-reactware` usage, the dev dependencies of
    `flight-reactware` would not be included and we would have a single copy
    of `react` under `flight-launch`es node_modules.
    
    However, when we `yarn link` `flight-reactware` into `flight-launch` we
    will end up with a copy of every installed dependency for
    `flight-reactware` including its dev dependencies.  In particular,
    multiple copies of `react` under `flight-launch`es node_modules.
    
    This commit ensures that only a single copy of `react` is included when
    building `flight-launch`.

--- client/config/config-overrides.dev.js
+++ client/config/config-overrides.dev.js
@@ -7,5 +7,8 @@ module.exports = function(config) {
   // Make CRA's "catchall" loader ignore .md files
   config.module.rules[1].exclude.push(/\.md$/);
 
+  // Avoids loading multiple copies of React
+ config.resolve.alias.react = path.resolve("./node_modules/react");
+
   return config;
 }
