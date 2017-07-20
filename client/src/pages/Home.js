a1e88a7f6c2f1b156938b4c318aa4b381f75929a
Ben Armston <ben.armston@alces-software.com>
2017-07-20 11:21:04 +0100

    Add copy to about pill
    This copy has been taken from the "WHAT IS THE ALCES FLIGHT LAUNCH
    SERVICE?" box and the welcome modal in the old client.  It has received
    minor editing to have them sit together a little better.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,24 +13,85 @@ const Home = () => {
       <Container fluid>
         <Row>
           <Col>
-            <h1>Hello.</h1>
-            <h2>This is an example.</h2>
+            <h1>Welcome to the Alces Flight Launch Service!</h1>
           </Col>
         </Row>
         <Row>
           <Col>
-            <Lorem count="2" />
+            <p>
+              On behalf of the Alces Flight Crew we’d like to welcome you on board the Alces
+              Flight Launch service. This service has been developed to quickly launch a
+              preconfigured High Performance Computing (HPC) cluster. With the Alces Flight
+              Launch service you gain temporary access to instant, single-user scientific
+              computing at no cost to you.
+            </p>
+
+            <h5>What is the Alces Flight Launch service?</h5>
+            <p>
+              The Alces Flight Launch service rapidly delivers a whole HPC
+              cluster, ready to go, complete with job scheduler and
+              applications.  Simply select the HPC cluster you want to
+              evaluate, enter your Flight Launch token, cluster name, and
+              an email address for notifications and you are ready to go!
+            </p>
+            <p>
+              Clusters are deployed in a Virtual Private Cluster (VPC) environment
+              for security, with SSH and graphical-desktop connectivity for
+              users.  Data management tools for POSIX and S3 object storage
+              are also included to help users transfer files and manage
+              storage resources.
+            </p>
+
+            <h5>How to use a Flight Launch token</h5>
+            <p>
+              Through one of the Alces Flight Crew or via our trusted partner
+              network you should have a received a Flight Launch token consisting of three
+              unrelated words (e.g. “flower – star – moose”). Don’t have one yet?  <a
+                href="mailto:flight@alces-flight.com?subject=Flight Launch Token
+                Request&body=Please send me a Flight Launch Token by return
+                email.%0D%0A%0D%0AKind regards." target="_blank" rel="noopener noreferrer">
+                Let us know</a> and we’ll start the process of securing you a Flight Launch
+              token.
+            </p>
+            <p>
+              Once you’ve secured a token simply search through the available preconfigured
+              clusters. When you’ve decided the HPC cluster you wish to trial enter the
+              token, the name you’d like to give your cluster, and the email address you
+              want to receive status updates to.
+            </p>
+            <p>
+              Then, simply press launch! You are now moments away from HPC cluster access.
+            </p>
           </Col>
           <Col>
-            <Lorem count="2" />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Lorem count="2" />
-          </Col>
-          <Col>
-            <Lorem count="2" />
+            <h5>Using your preconfigured HPC cluster</h5>
+            <p>
+              Once you are notified that your HPC cluster is active you can use the
+              pre-determined time as you see fit. Work with the applications that
+              come pre-installed or try your hand at installing and running
+              applications from the <a href="https://gridware.alces-flight.com/" target="_blank"
+                rel="noopener noreferrer">Alces Gridware open source application
+                library</a>.
+            </p>
+            <p>
+              While we’ll send you emails to let you know when your HPC cluster has
+              been launched, remember this is a trial service and is time
+              limited. Please remember to save any work you perform while using your
+              Flight Launch cluster!
+            </p>
+
+            <h5>Getting ready to launch and more information</h5>
+            <p>
+              Want to spend some time reading up on the Alces Flight Launch service prior to
+              starting your evaluation? We have a <a href="http://docs.alces-flight.com"
+                target="_blank" rel="noopener noreferrer">documentation site</a> dedicated to
+              the cause as well as a <a href="http://community.alces-flight.com"
+                target="_blank" rel="noopener noreferrer">Community Support Portal</a>{' '}
+              available for you to join in and read through.
+            </p>
+            <p>
+              Enjoy your flight!
+            </p>
           </Col>
         </Row>
       </Container>
