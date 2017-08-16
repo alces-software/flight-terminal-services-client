c21623a0414e93327e0d856d570427a0edb19672
Ben Armston <ben.armston@alces-software.com>
2017-08-16 16:38:24 +0100

    Easier configuration of fake cluster data

--- client/src/modules/clusters/actions.js
+++ client/src/modules/clusters/actions.js
@@ -50,37 +50,43 @@ function failedToLoad(error, ipAddress) {
   };
 }
 
-const fakeCluster = {
+const fakeClusterName = 'bewildered-iguana-lip-gloss';
+const fakeClusterHash = '0fbaf663';
+const fakeClusterHostname = `${fakeClusterName}-${fakeClusterHash}.cloud.alces.network`;
+
+const fakeCluster = (ipAddress) => ({
   id: 'd229b332-062e-4d91-b378-ea203f5a004c',
   type: 'clusters',
   attributes: {
-    browseConfigsUrl: 'https://solemnly-red-sloth-249e10c0.cloud.alces.network/vpn/downloads/',
+    browseConfigsUrl: `https://${fakeClusterHostname}/vpn/downloads/`,
+    clusterName: fakeClusterName,
     hasVpn: true,
-    hostname: 'solemnly-red-sloth-249e10c0.cloud.alces.network',
-    imgDir: 'https://solemnly-red-sloth-249e10c0.cloud.alces.network/vpn/img/',
-    ipAddress: '52.48.157.53',
+    hostname: fakeClusterHostname,
+    imgDir: `https://${fakeClusterHostname}/vpn/img/`,
+    ipAddress: ipAddress,
+    vpnConfigFiles: `https://${fakeClusterHostname}/vpn/downloads/clusterware`,
     vpnConfigs: [
       {
-        url: 'https://solemnly-red-sloth-249e10c0.cloud.alces.network/vpn/downloads/clusterware-openvpn.tar.gz',
+        url: `https://${fakeClusterHostname}/vpn/downloads/clusterware-openvpn.tar.gz`,
         os: 'linux',
       },
       {
-        url: 'https://solemnly-red-sloth-249e10c0.cloud.alces.network/vpn/downloads/clusterware-openvpn.zip',
+        url: `https://${fakeClusterHostname}/vpn/downloads/clusterware-openvpn.zip`,
         os: 'windows',
       },
       {
-        url: 'https://solemnly-red-sloth-249e10c0.cloud.alces.network/vpn/downloads/clusterware-tunnelblick.zip',
+        url: `https://${fakeClusterHostname}/vpn/downloads/clusterware-tunnelblick.zip`,
         os: 'macos',
       },
     ],
   },
-};
+});
 
 export function loadCluster(ipAddress) {
   return (dispatch) => {
     dispatch(loading(ipAddress));
     if (true) {
-      setTimeout(() => { dispatch(loaded(fakeCluster)); }, 1000);
+      setTimeout(() => { dispatch(loaded(fakeCluster(ipAddress))); }, 1000);
     } else {
       setTimeout(() => { dispatch(failedToLoad('fake error', ipAddress)); }, 1000);
     }
