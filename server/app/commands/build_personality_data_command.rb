fddc5eb9ecefdc62a42e3d54e2baa81d72fc5637
Ben Armston <ben.armston@alces-software.com>
2017-10-02 17:22:05 +0100

    Support non-solo clusters
     - The personality data needs to be built for `cluster-{master,compute}`
       stacks.
    
     - Care needs to be taken to ensure that we aren't adding new keys to
       the yaml files generated by `fly --create-parameter-dir`.
    
     - Configure compute and auth user for compute stacks.  The cluster name
       and auth user detected by `alces compute ...` does not work for
       clusters launched with Flight Launch, as the AWS stack name cannot be
       determined from the cluster name and domain name alone.
    
       Each cluster launched by Flight Launch includes a hash of the
       launcher's email address to prevent AWS stack name clashes.  We need
       to include this hash in the cluster name and auth user sent to
       tracon.

--- server/app/commands/build_personality_data_command.rb
+++ server/app/commands/build_personality_data_command.rb
@@ -22,6 +22,7 @@ class BuildPersonalityDataCommand
   def generate_personality_data
     personality = {}.tap do |h|
       h.merge!(generate_collections_data)
+      h.merge!(generate_compute_personality)
     end
     personality.to_yaml
   end
@@ -29,6 +30,30 @@ class BuildPersonalityDataCommand
   def generate_collections_data
     {
       'collections' => Array.wrap(@launch_config.collection)
+    }
+  end
+
+  def generate_compute_personality
+    hash = HashEmailCommand.new(@launch_config.email).perform
+    stack_name = "#{@launch_config.name}-#{hash}"
+
+    domain_arg_found = false
+    domain = nil
+    @launch_config.spec.args.each do |arg|
+      if domain_arg_found
+        domain = arg
+        break
+      end
+      if arg == '--domain' || arg == '-d'
+        domain_arg_found = true
+      end
+    end
+
+    {
+      'compute' => {
+        'cluster' => stack_name,
+        'auth_user' => "#{stack_name}.#{domain}.alces.network",
+      }
     }
   end
 end
