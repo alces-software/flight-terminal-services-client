fffcefe86d0f772f339c7ab2d56e18257bf39fe7
Ben Armston <ben.armston@alces-software.com>
2017-11-21 16:34:49 +0000

    WIP

--- client/src/modules/queueManagement/components/CurrentQueueCard.js
+++ client/src/modules/queueManagement/components/CurrentQueueCard.js
@@ -6,23 +6,14 @@ import {
   CardText,
   CardTitle,
 } from 'reactstrap';
+import styled, { css } from 'styled-components';
 
 import StatusText from './CardStatusText';
 import StatusIcon from './CardStatusIcon';
 import Buttons from './CardButtons';
 
-const propTypes = {
-  queue: PropTypes.shape({
-    status: PropTypes.string.isRequired,
-    spec: PropTypes.shape({
-      description: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-    }),
-  }).isRequired,
-};
-
-const CurrentQueueCard = ({ queue }) => (
-  <Card>
+const CurrentQueueCard = styled(({ className, queue }) => (
+  <Card className={className}>
     <CardBlock>
       <StatusIcon status={queue.status} />
       <CardTitle>
@@ -44,8 +35,27 @@ const CurrentQueueCard = ({ queue }) => (
       />
     </CardBlock>
   </Card>
-);
+))`
+  ${props => {
+    let colour = props.queue.status === 'UNCONFIGURED' ? '#f9f9f9' : '#fafff6';
+    return css`
+      background-color: ${colour};
+      background: repeating-linear-gradient(45deg,${colour},${colour} 5px,#ffffff 5px,#ffffff 8px);
+    `;
+  }}
+`;
 
-CurrentQueueCard.propTypes = propTypes;
+  // background-color: ${props => props.queue.status === 'UNCONFIGURED' ? 'grey' : 'green'};
+
+CurrentQueueCard.propTypes = {
+  className: PropTypes.string.isRequired,
+  queue: PropTypes.shape({
+    status: PropTypes.string.isRequired,
+    spec: PropTypes.shape({
+      description: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
 
 export default CurrentQueueCard;
