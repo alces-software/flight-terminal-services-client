e2b3022d9048513c009cb1849d5fc78ad0739db2
Ben Armston <ben.armston@alces-software.com>
2017-12-08 14:08:22 +0000

    Fix issue with top up form validations
    The underlying issue was to do with `react-router-config`'s
    `renderRoutes` and its nesting under `CSSTransitionGroup`.  This was
    causing the form to be mounted twice and unmounted once when the top up
    page was displayed.
    
    As redux-form's have side effects when mounted and unmounted the result
    of this was a broken form.  In particular, the validation was not
    working correctly.
    
    The solution was to ensure that `renderRoutes` uses the same `location`
    when transitioning out as it used when transitioning in.  This ensures
    that it will display the same components when transitioning out. The
    `renderRoutes` that is transitioning in will display the new components.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import Route from 'react-router/Route';
+import Switch from 'react-router/Switch';
 import { CSSTransitionGroup } from 'react-transition-group';
 import { compose } from 'recompose';
-import { matchRoutes, renderRoutes } from 'react-router-config';
+import { matchRoutes } from 'react-router-config';
 import { withRouter } from 'react-router';
 
 import { Page } from 'flight-reactware';
@@ -12,6 +14,52 @@ import ScrollToTop from './ScrollToTop';
 import SitePage from './Page';
 import routes from '../routes';
 import appVersion from '../version';
+
+
+// Use our own version of `renderRoutes` which incorporates currently
+// unreleased fixes from `react-router-config`'s `renderRoutes`.  The
+// important fix being the ability to pass props to the `Switch` used
+// internally by `renderRoutes`.
+//
+// This fix is important due to the nesting of `Switch` under
+// `CSSTransitionGroup`.  When a `Switch` is nested under
+// `CSSTransitionGroup`, we must make sure that it uses the same `location`
+// when transitioning out as it did when transitioning in.  If it doesn't, the
+// `Switch` which is transitioning out, will perform unnecessary mounting and
+// unmounting of its children due to different components matching the updated
+// location.
+//
+// At best, this results in unnecessary processing and a less smooth
+// animation.  If the components have a side-effect when mounted or unmounted,
+// something may be broken.
+//
+// We're currently using `react-router-config@1.0.0-beta.4`.  When a new
+// release is made, we can revert to using `react-router-config`'s
+// `renderRoutes`.
+//
+/* eslint-disable react/jsx-sort-props */
+/* eslint-disable react/jsx-max-props-per-line */
+/* eslint-disable react/jsx-tag-spacing */
+/* eslint-disable semi */
+const renderRoutes = (routes, extraProps = {}, switchProps = {}) => routes ? (
+  <Switch {...switchProps}>
+    {routes.map((route, i) => (
+      <Route
+        key={route.key || i}
+        path={route.path}
+        exact={route.exact}
+        strict={route.strict}
+        render={(props) => (
+          <route.component {...props} {...extraProps} route={route}/>
+        )}
+      />
+    ))}
+  </Switch>
+) : null
+/* eslint-enable react/jsx-sort-props */
+/* eslint-enable react/jsx-max-props-per-line */
+/* eslint-enable react/jsx-tag-spacing */
+/* eslint-enable semi */
 
 const productName = process.env.REACT_APP_PRODUCT_NAME;
 
@@ -46,7 +94,7 @@ const App = ({ location, route }) => {
             transitionName="fade"
           >
             <div key={location.key}>
-              {renderRoutes(route.routes)}
+              {renderRoutes(route.routes, {}, { location: location })}
             </div>
           </CSSTransitionGroup>
         </SitePage>
