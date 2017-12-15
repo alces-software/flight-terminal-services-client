d7bae6ce9c4b3dd72f274bee7ed222b59dfb36fb
Ben Armston <ben.armston@alces-software.com>
2017-12-15 16:47:48 +0000

    Rework Home page

--- manage/src/pages/Home.js
+++ manage/src/pages/Home.js
@@ -11,6 +11,7 @@ import {
 import styled from 'styled-components';
 import {
   LinkContainer,
+  PageHeading,
   Section,
   SectionIcon,
   makeSection,
@@ -20,12 +21,10 @@ import FontAwesome from 'react-fontawesome';
 import ContextLink from '../elements/ContextLink';
 import CommunitySiteLink from '../elements/CommunitySiteLink';
 import DocsSiteLink from '../elements/DocsSiteLink';
-import PageHeading from '../components/PageHeading';
 import { tenants } from '../modules';
 
 const sections = {
-  whatIsIt: makeSection('What is Flight Launch?', 'what-is-it', 'pink', 'question'),
-  howToUse: makeSection('How to use Flight Launch', 'how-to-use', 'orange', 'ticket'),
+  whatIsIt: makeSection('What is Flight Manage?', 'what-is-it', 'pink', 'question'),
   moreInfo: makeSection('Getting more information', 'more-information', 'blue', 'book'),
 };
 
@@ -59,91 +58,60 @@ const Home = ({ tenantIdentifier }) => {
     <div>
       <Container fluid>
         <PageHeading
-          overview="This service has been developed to quickly launch a
-          preconfigured High Performance Computing (HPC) cluster."
+          overview="This service has been developed to help access and manage
+          a Flight High Performance Computing (HPC) cluster."
           sections={Object.values(sections)}
-          title="Welcome to Alces Flight Launch!"
+          title="Welcome to Alces Flight Manage!"
         />
       </Container>
       <Container>
         <Section
-          overview="The Alces Flight Launch service rapidly delivers a whole
-          HPC cluster, ready to go, complete with job scheduler and
-          applications."
+          overview="The Alces Flight Manage service provides easy access to
+          your Flight HPC clusters and assistance with managing them."
           section={sections.whatIsIt}
-          title="What is Alces Flight Launch?"
-        >
-          <p>
-            Simply select the HPC cluster you want to evaluate, enter your
-            Flight Launch token, cluster name, and an email address for
-            notifications and you are ready to go!
-          </p>
-          <p>
-            Clusters are deployed in a Virtual Private Cluster (VPC)
-            environment for security, with SSH and graphical-desktop
-            connectivity for users.  Data management tools for POSIX and S3
-            object storage are also included to help users transfer files
-            and manage storage resources.
-          </p>
-        </Section>
-        <Section
-          overview="Use a Flight Launch token to take an Alces Flight Compute cluster for a spin — no cloud account required!"
-          section={sections.howToUse}
-          title="How to use Alces Flight Launch."
+          title="What is Alces Flight Manage?"
         >
           <Row>
             <Col>
-              <SectionIcon name="ticket" />
+              <SectionIcon name="plane" />
               <h4>
-                How to use a Flight Launch Token
+                Launch a Flight HPC cluster
               </h4>
               <p>
-                Through one of the Alces Flight Crew or via our trusted partner
-                network you should have a received a Flight Launch token
-                consisting of three unrelated words (e.g. “flower – star –
-                moose”). Don’t have one yet?{' '}
+                Simply use {' '}
                 <ContextLink
-                  linkSite="Home"
-                  location="/start#launch-flight"
+                  linkSite="Launch"
+                  location="/"
                 >
-                  Let us know
-                </ContextLink> and we’ll start the process of securing you a
-                Flight Launch token.
-              </p>
-              <p>
-                Once you’ve secured a token simply search through the available
-                preconfigured clusters. When you’ve decided the HPC cluster you
-                wish to trial enter the token, the name you’d like to give your
-                cluster, and the email address you want to receive status
-                updates to.
-              </p>
-              <p>
-                Then, simply press launch! You are now moments away from HPC
-                cluster access.
+                  Alces Flight Launch
+                </ContextLink>{' '}
+                to launch a preconfigured HPC cluster.  Once you are notified
+                that your HPC cluster is active follow the provided link to
+                gain access to your cluster and assistance with managing it.
               </p>
             </Col>
             <Col>
-              <SectionIcon name="server" />
+              <SectionIcon name="terminal" />
               <h4>
-                Using your preconfigured HPC cluster
+                Access your HPC cluster
               </h4>
               <p>
-                Once you are notified that your HPC cluster is active you can
-                use the pre-determined time as you see fit. Work with the
-                applications that come pre-installed or try your hand at
-                installing and running applications from the{' '}
-                <ContextLink
-                  linkSite="Gridware"
-                  location="/"
-                >
-                  Alces Gridware open source application library
-                </ContextLink>.
+                With Alces Flight Manage you can learn more about your cluster
+                with Flight Compute tutorials; gain access using the
+                in-browser terminal; or obtain the cluster's SSH and VPN
+                access details.
               </p>
+            </Col>
+            <Col>
+              <SectionIcon name="dashboard" />
+              <h4>
+                Manage your HPC cluster
+              </h4>
               <p>
-                While we’ll send you emails to let you know when your HPC
-                cluster has been launched, remember this is a trial service and
-                is time limited. Please remember to save any work you perform
-                while using your Flight Launch cluster!
+                Alces Flight Manage will assist you in managing your
+                long-running HPC clusters.  You can manage your cluster's
+                compute queues to ensure maximum efficiency at the lowest
+                cost.
               </p>
             </Col>
           </Row>
@@ -151,9 +119,9 @@ const Home = ({ tenantIdentifier }) => {
             <Col className="d-flex justify-content-center">
               <CallToAction
                 icon="play-circle"
-                to={`/${tenantIdentifier}/launch`}
+                to={'/cluster'}
               >
-                Launch a cluster now
+                Access your clusters now
               </CallToAction>
             </Col>
           </Row>
@@ -172,6 +140,7 @@ const Home = ({ tenantIdentifier }) => {
             {' '} available for you to join in and read through.
           </p>
           <p>
+            Alces Flight Launch is excellent you should try it out now!
             Enjoy your flight!
           </p>
         </Section>
