77652dbfba2b7ed6dc5df844d8a5edb2b3f908fa
Ben Armston <ben.armston@alces-software.com>
2018-01-31 17:09:53 +0000

    Add note about limitation and how to address it

--- manage/src/modules/clusters/components/withClusters.js
+++ manage/src/modules/clusters/components/withClusters.js
@@ -9,6 +9,20 @@ import { createStructuredSelector } from 'reselect';
 import * as selectors from '../selectors';
 // import LoadError from './LoadError';
 
+// XXX Show a spinner whilst waiting for retrievals to arrive.
+// XXX Avoid showing a spinner when reloading credit consumptions.
+// XXX Be more tolerant of load failures.
+//
+// All of the above are complicated by the way in which we identify clusters.
+// We sometimes use the `hostname` and sometimes use the `id`.  That works
+// fine when we are working with a single cluster, as we expect the user to
+// provide that cluster's hostname.  It doesn't work so well when we're
+// dealing with a list of clusters.
+//
+// When marketplace clusters are no longer supported by Flight Manage, we can
+// change to using an `id` consistently.  That will make fixing the above
+// issues much simpler.  Until then, we're going to have to live with them.
+//
 const withClusters = compose(
   connect(createStructuredSelector({
     clusters: selectors.clustersConsumingCredits,
